65286555d4b1427262e8343f4819516adafb5655
Gigolaev Igor <41339681+Gigaw@users.noreply.github.com>
2024-02-25 14:01:19 +0400

    start

---
+++ src/components/Spacer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Spacer = ({ height, width }) => {
+  return <View style={{ height: height || 10, width: width || 10 }} />;
+};
+
+export default Spacer;
